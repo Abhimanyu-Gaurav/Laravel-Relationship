396cd7113f8ed528ed9225d820ba57caf3d8fbb6
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-10-30 12:24:02 +0530

    create controller, models & migrations

---
+++ database/migrations/2024_10_30_064859_create_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained()->onDelete('cascade'); // Foreign key referencing sellers
+            $table->string('product_name'); // Product name
+            $table->decimal('price', 8, 2); // Product price
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
